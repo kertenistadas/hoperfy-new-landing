cdc49358fbd9678731be3933e8790bc8990bba1d
Tadas <tadas@tedo.lt>
2026-08-06 20:34:31 +0300

    Add Google Analytics GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({
@@ -91,6 +92,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             })
           }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LBTP0CYHL4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LBTP0CYHL4');
+          `}
+        </Script>
       </body>
     </html>
   )
