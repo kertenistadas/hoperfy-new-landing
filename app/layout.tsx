c80218a0e118551d6bb8b9cdb9a1cbdff82659b2
Tadas <tadas@tedo.lt>
2026-08-07 12:17:10 +0300

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,8 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://hoperfy.com'),
   alternates: { canonical: 'https://hoperfy.com' },
   icons: {
-    icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
-    ],
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
     apple: '/favicon-180x180.png',
   },
   openGraph: {
